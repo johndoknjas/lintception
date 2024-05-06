2d4b4dd4d08271b787e967ce49d9a294c3575472
johndoknjas <jdoknjas@sfu.ca>
2024-05-05 21:32:52 -0700

    In `linters.py`, have the individual test functions return a bool (True if no errors), and have the `run_linters` function return an Enum member. This is to help these functions be useful when called from other scripts.

--- lintception/linters.py
+++ lintception/linters.py
@@ -4,29 +4,33 @@ from subprocess import PIPE
 import glob
 import vulture # type: ignore
 import mypy.api
+from enum import Enum
 
 from . import Utils
 
-def test_vulture() -> None:
+class LintResult(Enum):
+    SUCCESS, MYPY_ERR, VULTURE_ERR, VERMIN_ERR, NO_FUTURE_ANNOT_ERR = range(5)
+
+def test_vulture() -> bool:
     v = vulture.Vulture()
     v.scavenge(['.'])
-    assert not v.get_unused_code()
+    return not v.get_unused_code()
     # https://stackoverflow.com/a/59564370/7743427
 
-def test_mypy() -> None:
-    assert mypy.api.run(['.']) == (
+def test_mypy() -> bool:
+    return mypy.api.run(['.']) == (
         f'Success: no issues found in {Utils.num_python_files()} source files\n', '', 0
     )
     # https://mypy.readthedocs.io/en/stable/extending_mypy.html#integrating-mypy-into-another-python-application
 
-def test_vermin(settings: dict) -> None:
+def test_vermin(settings: dict) -> bool:
     result = subprocess.run(['vermin', '.'], stdout=PIPE, stderr=PIPE, universal_newlines=True)
     expected_ending = (f"Minimum required versions: {settings['MinVersion']}\n" +
                        f"Incompatible versions:     {settings['NumIncompatibleVersions']}")
-    assert result.stdout.strip().endswith(expected_ending)
-    assert (result.returncode, result.stderr) == (0, '')
+    return (result.stdout.strip().endswith(expected_ending) and
+            (result.returncode, result.stderr) == (0, ''))
 
-def test_future_annotations() -> None:
+def test_future_annotations() -> bool:
     for filename in glob.iglob('**/*.py', recursive=True):
         assert filename.endswith(".py")
         with open(filename) as file:
@@ -34,14 +38,17 @@ def test_future_annotations() -> None:
                 (line.rstrip('\n') for line in file.readlines() if Utils.is_code_line(line)), None
             )
             if filename.endswith('__init__.py'):
-                assert first_code_line is None
-            else:
-                assert first_code_line == "from __future__ import annotations"
+                if first_code_line is not None:
+                    return False
+            elif first_code_line != "from __future__ import annotations":
+                return False
+    return True
 
-def run_linters(settings: dict) -> None:
+def run_linters() -> LintResult:
+    settings: dict[str, float | int] = {'MinVersion': 3.8, 'NumIncompatibleVersions': 2}
+    settings.update(Utils.read_json_file('.lintception'))
     Utils.assertions_for_settings_dict(settings)
-    test_vulture()
-    test_mypy()
-    test_vermin(settings)
-    print('vulture, mypy, and vermin found no errors!\n')
-    test_future_annotations()
+    tests = ((test_vulture, LintResult.VULTURE_ERR), (test_mypy, LintResult.MYPY_ERR),
+             (lambda: test_vermin(settings), LintResult.VERMIN_ERR),
+             (test_future_annotations, LintResult.NO_FUTURE_ANNOT_ERR))
+    return next((x[1] for x in tests if not x[0]()), LintResult.SUCCESS)
--- lintception/main.py
+++ lintception/main.py
@@ -1,15 +1,16 @@
 from __future__ import annotations
+import sys
 
 from . import linters
 from . import my_linter
-from . import Utils
 
 def main() -> None:
     if not __debug__:
         raise RuntimeError("Python isn't running in the default debug mode.")
-    settings: dict[str, float | int] = {'MinVersion': 3.8, 'NumIncompatibleVersions': 2}
-    settings.update(Utils.read_json_file('.lintception'))
-    linters.run_linters(settings)
+    if (result := linters.run_linters()) != linters.LintResult.SUCCESS:
+        print(f"Error: {result.name}")
+        sys.exit(0)
+    print('vulture, mypy, and vermin found no errors. Also, no python files without a future annotations import were found.\n')
     my_linter.main()
 
 if __name__ == '__main__':
